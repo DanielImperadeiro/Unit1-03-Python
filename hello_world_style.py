d8bfbbf1b5032d0eb14aa5807e6a00f5101c76d2
Ubuntu <ubuntu@ip-172-31-15-252.ec2.internal>
2022-02-22 14:25:32 +0000

    Added the code

---
+++ hello_world_style.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# Created By: Daniel Imperadeiro
+# Date: Feb. 22, 2022
+# This program displays Hello, World! with proper style.
+
+def main():
+    print ("Hello, World! with proper style!")
+
+
+if __name__ == "__main__":
+    main()
